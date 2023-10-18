e04d395dc54db125c2bab44f84ee8ce0f33b5713
kimmeh <mhko0411@gmail.com>
2023-10-18 21:47:53 +0900

    feat: copy 이벤트에 디바운싱 적용

--- src/background.js
+++ src/background.js
@@ -1,27 +1,25 @@
 //background.js
 chrome.runtime.onInstalled.addListener(() => {
-  
-  chrome.storage.sync.get("copyList").then(function(result) {
-
-    for(let i = 0; i < result.copyList.length; i++) {
-
-      const copyList = result.copyList || [];
-      updateContextMenu(copyList);
-    }
-  });
+  // error code
+  // chrome.storage.sync.get("copyList").then(function(result) {
+  //   for(let i = 0; i < result.copyList.length; i++) {
+  //     const copyList = result.copyList || [];
+  //     updateContextMenu(copyList);
+  //   }
+  // });
 });
 
 // 컨텍스트 메뉴 업데이트 함수
 function updateContextMenu(copyList) {
   // 기존에 생성된 컨텍스트 메뉴들을 삭제
   chrome.contextMenus.removeAll();
-  
+
   // 새로운 컨텍스트 메뉴 생성
   chrome.contextMenus.create({
     title: "Copy-Keeper",
     id: "Copy",
     contexts: ["all"],
-    visible: false
+    visible: false,
   });
 
   // 저장된 클립보드 데이터들을 반복하여 컨텍스트 메뉴 생성
@@ -29,60 +27,56 @@ function updateContextMenu(copyList) {
     chrome.contextMenus.create({
       title: copyList[i].text,
       id: "Copy" + i,
-      contexts: ["all"]
+      contexts: ["all"],
     });
   }
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-
   if (info.menuItemId.startsWith("Copy")) {
     // 선택한 메뉴의 id에 따라 처리
     let index = parseInt(info.menuItemId.replace("Copy", ""));
     handleCopy(index, tab);
   }
-    
 });
-  
+
 function handleCopy(index, tab) {
-  chrome.storage.sync.get("copyList").then(function(result) {
+  chrome.storage.sync.get("copyList").then(function (result) {
     let testData = result.copyList;
-    
+
     if (index >= 0 && index < testData.length) {
       var selectedValue = testData[index].text;
 
       try {
         copyToClipboard(selectedValue, tab.id);
-      } catch(error) {
-          console.error(error);
+      } catch (error) {
+        console.error(error);
       }
     }
   });
 }
-  
+
 async function copyToClipboard(selectedValue, tabId) {
   try {
     await chrome.scripting.executeScript({
       target: { tabId: tabId, allFrames: true },
-      function: function(selectedValue) {
-        const dummyElement = document.createElement('textarea');
+      function: function (selectedValue) {
+        const dummyElement = document.createElement("textarea");
         dummyElement.value = selectedValue;
         document.body.appendChild(dummyElement);
         dummyElement.select();
-        document.execCommand('copy');
+        document.execCommand("copy");
         document.body.removeChild(dummyElement);
-
-      }, args: [selectedValue]
+      },
+      args: [selectedValue],
     });
-
   } catch (error) {
-    console.error('스크립트 실행 중 오류가 발생했습니다.', error);
+    console.error("스크립트 실행 중 오류가 발생했습니다.", error);
   }
 }
 
 // 컨텐츠 스크립트에서 클립보드 데이터를 수신하는 메시지 핸들러 등록
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-
   if (message.type === "copy_event") {
     const { text, id } = message;
     handleEvent(text, "add");
@@ -99,7 +93,9 @@ async function handleEvent(data, status) {
     let copyList;
 
     if (status == "add") {
-      const { copyList: addCopyList } = await chrome.storage.sync.get("copyList");
+      const { copyList: addCopyList } = await chrome.storage.sync.get(
+        "copyList"
+      );
       copyList = addCopyList;
     } else if (status == "del") {
       copyList = data;
@@ -111,4 +107,3 @@ async function handleEvent(data, status) {
     console.error("Background Error: ", e);
   }
 }
-  
